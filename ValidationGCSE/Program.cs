9d98326159479d6ff3a421b5e93f14ed9a4d8fb7
Tony Collins <tjc@eltham-college.org.uk>
2020-12-01 14:25:59 +0000

    Add project files.

---
+++ ValidationGCSE/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ValidationGCSE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Validation Tasks- write the validation routines described below. You will have to use 
+            // indefinte iteration, not for loops!
+
+            // LENGTH
+            // 1) user should enter a password(string) with more than 4 characters and less than 10
+            // keep prompting the user for a password until they enter one of a valid length
+
+
+
+            // RANGE
+            //2) User should enter an number greater than 5 and less than 10, continue to prompt the user
+            // until they enter a valid number
+
+
+
+            //3) User should enter a even number, continue to promt the uer until they enter an even number
+
+
+
+
+            //FORMAT
+            //4 User should enter a string that contains a @ and a .(full stop) continue to prompt the
+            // user until they enter a valid number.
+
+
+
+
+            //Verification
+            // User should enter their password twice, the password should be verified.
+            // if user does not enter the same password twice they should continue to be
+            //prompted for their password
+
+
+            // EXTENSION TYPE
+            // User should enter a double(as a string. The program should continue to
+            // prompt the user until they enter a string that can be converted to a double
+            // eg hello is invalid, 47.89 is valid
+
+
+
+
+
+
+        }
+    }
+}
